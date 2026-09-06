96c963a20755ea270ef49a738b21de140fd6c224
Manoo Abhillash <manoo.abhillash@gmail.com>
2026-09-06 12:46:01 +0530

    changes the console log for testing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+     console.log(`Server is running on http://localhost:${PORT}`);
 });
